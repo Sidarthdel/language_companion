adc93c5802026e1f7f4b01b5b3f551e7a4b93ded
Sidharth Deleep <sidharthdeleep@gmail.com>
2024-04-12 14:49:42 +0530

    some changes

--- app/(root)/(routes)/page.tsx
+++ app/(root)/(routes)/page.tsx
@@ -1,12 +1,17 @@
 import { SearchInput } from "@/components/search-input";
+import prismadb from "@/lib/prismadb";
+import { Categories } from "@/components/categories";
 
-const RootPage =() =>{
-return  (
+const RootPage =async () =>{
 
-<div className="h-full p-4 space-y-2">
- <SearchInput/>
-</div>
-        )
-}
+        const categories = await prismadb.category.findMany();
+
+        return(
+        <div className="h-full p-4 space-y-2">
+        <SearchInput/>
+        <Categories data={categories}/>
+        </div>
+        );
+        }
 
 export default RootPage; 
---
+++ app/(root)/companion/[companionId]/components/companion-form.tsx
@@ -0,0 +1,90 @@
+"use client"
+
+import {Companion,Category} from "@prisma/client";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import {zodResolver} from "@hookform/resolvers/zod";
+
+
+import { Form, FormField, FormItem, FormControl, FormMessage } from "@/components/ui/form";
+import { Separator } from "@/components/ui/separator";
+
+
+interface CompanionFormProps{
+    initialData:Companion | null;
+    categories:Category[];
+}
+
+const formSchema = z.object({
+    name:z.string().min(1,{
+        message:"Name is required",
+    }),
+    description:z.string().min(1,{
+        message:"Description is required",
+    }),
+    instructions:z.string().min(200,{
+        message:"Intructions require at least 200 characters",
+    }),
+    seed:z.string().min(1,{
+        message:"Seed require at least 200 characters",
+    }),
+    src:z.string().min(1,{
+        message:"Image is required",
+    }),
+    categoryId:z.string().min(1,{
+        message:"Category is required",
+    }),
+})
+
+
+export const CompanionForm =({
+    categories,initialData
+}: CompanionFormProps
+) =>{
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver:zodResolver(formSchema),
+        defaultValues:initialData || {
+            name:"",
+            description:"",
+            instructions:"",
+            seed:"",
+            src:"",
+            categoryId: undefined,
+        },
+    });
+
+    const isLoading = form.formState.isSubmitting;
+
+    const onSubmit = async(values:z.infer<typeof formSchema>) =>{
+        console.log(values);
+    }
+    return(
+      <div className="h-full p-4 space-y-2 max-w-3xl mx-auto">
+        <Form {...form}>
+        <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 pb-10">
+        <div className="space-y-2 w-full ">
+        <div className="">
+            <h3 className="text-lg font-medium">
+                Generation Information
+            </h3>
+            <p className="text-sm text-muted-foreground">
+                Gives the information of your companion
+            </p>
+        </div>
+        <Separator className="bg-primary/10"/>
+        </div>
+        <FormField name="src"
+        render={({field}) =>(
+            <FormItem className="flex flex-col items-center justify-center space-y-4 ">
+                <FormControl>
+                imageupload
+                </FormControl>
+                <FormMessage/>
+            </FormItem>
+        )}
+        />
+        </form>
+        </Form>
+      </div>  
+    )
+}
